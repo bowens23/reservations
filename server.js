eb675d32d80b8fe3544f790cd24731b6a88b432a
Benjamin Owens <Benjamin.Owens@amtrustgroup.com>
2017-07-22 11:57:27 -0400

    second commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var reservation = [{
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/table", function(req, res) {
